1f46e5dcb4c454d0250eb1fde98ebc394a87a6fd
melchristian07 <christianquidet07@gmail.com>
2022-07-23 12:25:52 +0800

    quiz

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+const questions = [
+    {
+    numb: 1,
+    question: "Who found Diariong Tagalog?",
+    answer: "Marcelo del Pilar",
+    options: [
+      "Juan Luna",
+      "Marcelo del Pilar",
+      "Pedro Laktaw",
+      "Gregorio Sanciano"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Taga-Ilog was the pen name which was associated with:",
+    answer: "Antonio Luna",
+    options: [
+      "Juan Luna",
+      "Mariano Ponce",
+      "Jose Ma. Panganiban",
+      "Antonio Luna"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The political section of the Asociacion Hispano-Filipino was under the charge of:",
+    answer: "Marcelo del Pilar",
+    options: [
+      "Jose Rizal",
+      "Marcelo del Pilar",
+      "Dominador Gomez",
+      "Thomas Arejola"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these is not true of the La Solidaridad?",
+    answer: "It featured articles about the good deeds of the friars.",
+    options: [
+      "It was a forum for discussion of the issues concerning the Philippines.",
+      "It was the official organ of the reform movement.",
+      "It was a vehicle for the expression of the political views of the reformers.",
+      "It featured articles about the good deeds of the friars."
+    ]
+  },
+    {
+    numb: 5,
+    question: "What did The First Philippine Republic came to be called?",
+    answer: "Malolos Republic",
+    options: [
+      "Biak-na-Bato Republic",
+      "Sakay Republic",
+      "Malolos Republic",
+      "Non of the above"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Because of which reason, was Rizal not in favor of the revolution planned by the Katipunan?",
+    answer: "Unpreparedness of the Katipuneros for an armed struggle",
+    options: [
+      "Unpreparedness of the Katipuneros for an armed struggle",
+      "Willingness to the wealthy Filipinos to support the struggle",
+      "Absence of a military tactician who will direct the military operations of the KKK",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Based on the plan proposed by whom was The Malolos Constitution drafted? ",
+    answer: "Calderon",
+    options: [
+      "Paterno",
+      "Calderon",
+      "Mabini",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What was The rightist wing of La Liga called?",
+    answer: "Cuerpos de Compromisarios",
+    options: [
+      "Cuerpos de Propagandistas",
+      "Cuerpos de Compromisarios",
+      "Cuerpos de Militantes",
+      "Cuerpos de la Revolucionarios"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Tell the name of the president of the Associacion La Solidaridad.",
+    answer: "Galiciano Apacible",
+    options: [
+      "Galiciano Apacible",
+      "Mariano Ponce",
+      "Manuel Sta. Maria",
+      "Jose Ma. Panganiban"
+    ]
+  },
+    {
+    numb: 10,
+    question: "The first real battle fought between Filipinos and the Spaniards is known as:",
+    answer: "Pinaglabanan",
+    options: [
+      "Majayjay",
+      "Pugad Lawin",
+      "Pinaglabanan",
+      "Montalban"
+    ]
+  },
+];
